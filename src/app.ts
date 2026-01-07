a5bca5bb37e70e08d51d64164b087e619156b256
Ankit Nayak <ankitnayak882@gmail.com>
2026-01-07 15:51:44 +0530

    Refactor app.ts to include request logger middleware and improve code formatting

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
-import express from "express"
-import { errorHandler } from "./middlewares/error.middleware"
-import { notFound } from "./middlewares/notFound.middleware"
-import healthRouter from "./routes/health.route"
+import express from "express";
+import { errorHandler } from "./middlewares/error.middleware";
+import { notFound } from "./middlewares/notFound.middleware";
+import healthRouter from "./routes/health.route";
+import { requestLogger } from "./middlewares/requestLogger.middleware";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use("/health", healthRouter)
+app.use(requestLogger);
+app.use(express.json());
+app.use("/health", healthRouter);
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
