08658beecf0b0d48ef2fe72113bfc7196fd3575e
Ankit Nayak <ankitnayak882@gmail.com>
2026-01-07 15:51:05 +0530

    Refactor error handling middleware to include custom ApiError handling and logging for unhandled errors

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,10 +1,24 @@
-import { Request,Response,NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
+import { ApiError } from "../utils/apiError";
+import { logger } from "../utils/logger";
 
-export const errorHandler = (err: Error, req: Request, res: Response,next:NextFunction) => {
-    console.log(err)
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof ApiError) {
+    return res.status(err.statusCode).json({
+      success: false,
+      message: err.message,
+    });
+  }
 
-    res.status(500).json({
-        success: false,
-        message: err.message || "Internal Server Error"
-    })
-}
+  logger.error(`Unhandled Error: ${err}`);
+
+  res.status(500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
+};
